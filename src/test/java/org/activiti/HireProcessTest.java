1457a10ab376eb85e195b7530f976d2b7dc352b0
neo-feng <neo.feng@outlook.com>
2018-05-02 23:47:39 +0800

    add

--- src/test/java/org/activiti/HireProcessTest.java
+++ src/test/java/org/activiti/HireProcessTest.java
@@ -1,8 +1,6 @@
 package org.activiti;
 
-import org.activiti.engine.HistoryService;
-import org.activiti.engine.RuntimeService;
-import org.activiti.engine.TaskService;
+import org.activiti.engine.*;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.After;
@@ -106,5 +104,28 @@ public class HireProcessTest {
         Assert.assertEquals(1, historyService.createHistoricProcessInstanceQuery().finished().count());
 
     }
-
+    @Test
+    public void creteTable() {
+        ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration
+                .createStandaloneProcessEngineConfiguration();
+        // 连接数据库的配置
+        // 配置数据库驱动:对应不同数据库类型的驱动
+        processEngineConfiguration.setJdbcDriver("com.mysql.jdbc.Driver");
+        // 配置数据库的JDBC URL
+        processEngineConfiguration
+                .setJdbcUrl("jdbc:mysql://localhost:3306/activiti?useUnicode=true&characterEncoding=utf8");
+        // 配置连接数据库的用户名
+        processEngineConfiguration.setJdbcUsername("root");
+        // 配置连接数据库的密码
+        processEngineConfiguration.setJdbcPassword("github");
+        /**
+         * public static final String DB_SCHEMA_UPDATE_FALSE = "false";不能自动创建表，需要表存在
+         * public static final String DB_SCHEMA_UPDATE_CREATE_DROP = "create-drop";先删除表再创建表
+         * public static final String DB_SCHEMA_UPDATE_TRUE = "true";如果表不存在，自动创建表
+         */
+        processEngineConfiguration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        // 工作流的核心对象，ProcessEnginee对象
+        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+        System.out.println("processEngine:" + processEngine);
+    }
 }
